84cf1f938c6956e04cc98ef1cd74fb634551e84c
yaghoub <y.ghaderi@outlook.com>
2023-12-28 02:56:34 +0330

    dev get data from codal.ir

---
+++ oxtapus/codal.py
@@ -0,0 +1,105 @@
+from typing import Literal
+from pydantic import BaseModel, ConfigDict, field_serializer
+from tarix import Date
+
+url_main = "https://codal.ir"
+search_url = "/ReportList.aspx?search"
+api = "api/search/v2/q"
+
+
+def to_camel(string: str) -> str:
+    return ''.join(word.capitalize() for word in string.split('_'))
+
+
+class QueryParam(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    symbol: str
+    category: Literal[1] = 1  # گروه اطلاعیه --> اطلاعات و صورت مالی سالانه
+    publisher_type: Literal[1] = 1  # نوع شرکت --> ناشران
+    letter_type: Literal[6] = 6  # نوع اطلاعیه --> اطلاعات و صورتهای مالی میاندوره ای
+    length: Literal[3, 6, 9, 12]  # طول دوره
+    audited: bool = True  # حسابرسی شده
+    not_audited: bool = True  # حسابرسی نشده
+    mains: bool = True  # فقط شرکت اصلی
+    childs: bool = False  # فقط زیر-مجموعه‌ها
+    consolidatable: bool = True  # اصلی
+    not_consolidatable: bool = True  # تلفیقی
+
+    auditor_ref: Literal[-1] = -1
+    company_state: Literal[1] = 1
+    Company_type: Literal[1] = 1
+    page_number: int = 1
+    tracing_no: Literal[-1] = -1
+    publisher: bool = False
+    is_not_audited: bool = False
+    from_date: str = "1396/01/01"
+
+    @field_serializer('audited', 'not_audited', 'mains', 'childs', 'consolidatable', 'not_consolidatable', 'publisher',
+                      'is_not_audited')
+    def serialize_bool(self, v: bool):
+        return str(v).lower()
+
+
+def norm_char(w: str):
+    dict_ = {
+        "ي": "ی",
+        "ك": "ک",
+        "\u200c": "",
+        "۰": "0",
+        "۱": "1",
+        "۲": "2",
+        "۳": "3",
+        "۴": "4",
+        "۵": "5",
+        "۶": "6",
+        "۷": "7",
+        "۸": "8",
+        "۹": "9",
+        "/": "-"
+    }
+    return w.translate(str.maketrans(dict_))
+
+
+def jalali_to_gregorian(dtstr: str):
+    """1402-05-12 12:20:42"""
+    date = Date(dtstr[:10]).jalali_to_gregorian().strftime("%Y-%m-%d")
+    time = dtstr[-8:]
+    return f"{date} {time}"
+
+
+class Letters(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+
+    tracing_no: int
+    symbol: str
+    company_name: str
+    title: str
+    letter_code: str
+    sent_date_time: str
+    publish_date_time: str
+    has_html: bool
+    is_estimate: bool
+    has_excel: bool
+    has_pdf: bool
+    has_xbrl: bool
+    has_attachment: bool
+    url: str
+    attachment_url: str
+    pdf_url: str
+    excel_url: str
+    xbrl_url: str
+
+    @field_serializer('symbol', 'company_name', "letter_code", "title")
+    def serialize_norm_char(self, v: str):
+        return norm_char(v)
+
+    @field_serializer('sent_date_time', 'publish_date_time')
+    def serialize_datetime(self, v: str):
+        return jalali_to_gregorian(norm_char(v))
+
+    @field_serializer('url', "attachment_url", "pdf_url")
+    def serialize_url(self, v: str):
+        if v[0] != "/":
+            v = f"/{v}"
+        return f"{url_main}{v}"
