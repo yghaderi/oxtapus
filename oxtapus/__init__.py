13c6af693200dd7765e19c750aa39aa22e3f0fa9
Yaghoub <y.ghaderi@outlook.com>
2025-05-09 08:33:02 +0330

    handle warn __init__

--- oxtapus/__init__.py
+++ oxtapus/__init__.py
@@ -1,3 +1,6 @@
 from oxtapus.tsetmc import TSETMC
 from oxtapus.rahavard import Rahavard
 from oxtapus.tgju import TGJU
+from oxtapus.fipiran import Fipiran
+
+__all__ = ["TSETMC", "Rahavard", "TGJU", "Fipiran"]
